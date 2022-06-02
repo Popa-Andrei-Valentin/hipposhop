464ce8a16d395146e5f43eee7dc32c08345d5fbf
BALICA Ionut <Ionut.BALICA@cegedim.com>
2022-06-02 15:32:55 +0300

    product component structure created and it dispays image, title, price quantity and add to cart butteb

---
+++ src/components/ProductComp.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <div class="card">
+      <img
+        v-if="image === null"
+        src="http://www.womens-southerngolfassociation.org/wp-content/uploads/2021/10/Image-Not-Available.png"
+        alt="{{ name }}"
+      />
+      <img v-else :src="image" v-bind:alt="name" />
+      <div>
+        <p>{{ name }}</p>
+      </div>
+      <div>
+        <p>Preț:</p>
+        <p>{{ price }} / {{ unit }}</p>
+      </div>
+
+      <div>
+        <p>Cantitate:</p>
+
+        <input class="cell" type="number" v-model.number="quantity" />
+      </div>
+      <div></div>
+
+      <div>
+        <button>Add to cart</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["image", "name", "price", "unit"],
+  // image: {
+  //   type: String,
+  //   default: null,
+  // },
+  // name: {
+  //   type: String,
+  //   required: true,
+  // },
+  // price: {
+  //   type: Number,
+  //   required: true,
+  // },
+  // unit: {
+  //   type: String,
+  //   required: true,
+  // },
+  data() {
+    return {
+      quantity: 0,
+    };
+  },
+};
+</script>
+
+<style>
+.card {
+  display: flex;
+  flex-direction: row;
+  min-height: 3rem;
+  align-items: flex-start;
+  justify-content: flex-start;
+  background: #fbf7ec;
+  border: 0.5rem solid white;
+  padding: 0.5rem;
+  font-family: "Open Sans", sans-serif;
+}
+.card p {
+  position: relative;
+  display: flex;
+  width: 8rem;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding-left: 1rem;
+}
+
+img {
+  width: 8rem;
+  height: 6rem;
+}
+.cell {
+  align-items: center;
+  width: 2rem;
+
+}
+</style>
--- src/components/ProductPage.vue
+++ src/components/ProductPage.vue
@@ -1,115 +1,131 @@
 <template>
+  <h3>Products</h3>
+  <div class="breadCrumb">
+    <div v-for="items in breadCrumb" :key="items.id">
+      <a @click="clickBread(items)">{{ items.name }}</a>
+      <!-- **item is not the last -->
+      <span class="separator" v-if="items != breadCrumb[breadCrumb.length - 1]">
+        |
+      </span>
+    </div>
+  </div>
 
-	<h3>Products</h3>
-	<div class="breadCrumb">
-		<div  v-for="items in breadCrumb" :key="items.id">
-			<a @click="clickBread(items)" >{{ items.name }}</a>
-		<!-- **item is not the last -->
-			<span class="separator" v-if="items != breadCrumb[breadCrumb.length-1]"> | </span>
-		</div>
-	</div>
-		
-	<div class="product-box">
-		<!-- Loop and display products -->
-		<ol>
+  <div class="product-box">
+    <ol class="product">
+      <div>
+        <ProductComp
+          v-for="product in products"
+          :key="product.id"
+          :name="product.title"
+          :price="product.price"
+          :image="product.image"
+          :unit="product.unit"
+        />
+      </div>
+    </ol>
+    <!-- Loop and display products -->
+    <!-- <ol>
 			<li class="product" v-for="product in products" :key="product.id">
 				{{ `${product.title} - pret:
 								${product.price} lei / ${[product.unit]} `
 				}}
 			</li>
-		</ol>
-		<!-- Loop and display category_id filtered products -->
-		<!--        <ol v-else>-->
-		<!--            <li class="product" v-for="product in products.filter(product => product.category_id === categoryId)"-->
-		<!--                :key="product.id">{{ `${product.title} - pret: ${product.price} lei / ${[product.unit]} ` }}</li>-->
-		<!--        </ol>-->
-	</div>
-
+		</ol> -->
+    <!-- Loop and display category_id filtered products -->
+    <!--        <ol v-else>-->
+    <!--            <li class="product" v-for="product in products.filter(product => product.category_id === categoryId)"-->
+    <!--                :key="product.id">{{ `${product.title} - pret: ${product.price} lei / ${[product.unit]} ` }}</li>-->
+    <!--        </ol>-->
+  </div>
 </template>
 
 <script>
-export default {
+import ProductComp from "./ProductComp.vue";
 
-	props: {
-		categoryId: {
-			type: Number,
-			default: () => 0,
-		},
-		breadCrumb: {
-			type: Array,
-			default: () => ["Toate produsele"],
-		},
-	},
-	name: "ProductsComponent",
-	emits: ["breadCrumbSelect","breadCrumbUpdate"],
-	computed: {
-		//  Retrieve list from LocalStorage.
-		products() {
-			if (this.categoryId) {
-				return this.$store.getters["products/getList"].filter(
-					(product) =>
-						(String(product.category_id) + ",")
-							.split(",")
-							.includes(String(this.categoryId)) === true
-				);
-			} else {
-				return this.$store.getters["products/getList"];
-			}
-		},
-	},
-	mounted() {
-		// Mounts LocalStorage list.
-		this.$store.dispatch("products/loadList", this.data);
-	},
-	methods: {
-		clickBread(item) {
-			this.$emit("breadCrumbSelect", item);
-		},
-	}
+export default {
+  components: {
+    ProductComp,
+  },
+  props: {
+    categoryId: {
+      type: Number,
+      default: () => 0,
+    },
+    breadCrumb: {
+      type: Array,
+      default: () => ["Toate produsele"],
+    },
+  },
+  name: "ProductsComponent",
+  emits: ["breadCrumbSelect", "breadCrumbUpdate"],
+  computed: {
+    //  Retrieve list from LocalStorage.
+    products() {
+      if (this.categoryId) {
+        return this.$store.getters["products/getList"].filter(
+          (product) =>
+            (String(product.category_id) + ",")
+              .split(",")
+              .includes(String(this.categoryId)) === true
+        );
+      } else {
+        return this.$store.getters["products/getList"];
+      }
+    },
+  },
+  mounted() {
+    // Mounts LocalStorage list.
+    this.$store.dispatch("products/loadList", this.data);
+  },
+  methods: {
+    clickBread(item) {
+      this.$emit("breadCrumbSelect", item);
+    },
+  },
 };
 </script>
 
 <style scoped>
 ol {
-	display: flex;
-	flex-direction: column;
-	align-items: flex-start;
-	overflow: hidden;
-	overflow-y: scroll;
-	height: 33vh;
-	width: 60ch;
-	border: 2px solid black;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  overflow: hidden;
+  overflow-y: scroll;
+  height: 50vh;
+  width: auto;
+  border: 2px solid black;
 }
 
 li {
-	padding-top: 0.3rem;
+  padding-top: 0.3rem;
 }
 
 .product-box {
-	padding-left: 2rem;
+  padding-left: 2rem;
 }
 
 .product {
-	word-spacing: 0.1rem;
-	padding-block: 0.4rem;
+  word-spacing: 0.1rem;
+  padding-block: 0.4rem;
 }
 
-.breadCrumb{
-	display:flex;
-	flex-direction: row;
-	align-items: flex-start;
-	font-weight: bold;
-	font-style: italic;
-	padding-left: 1.5rem;
+.breadCrumb {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  font-weight: bold;
+  font-style: italic;
+  padding-left: 1.5rem;
 }
 
-.breadCrumb a{
-	padding-left: 0.3rem;
-	padding-right: 0.3rem;
-	cursor: pointer;
+.breadCrumb a {
+  padding-left: 0.3rem;
+  padding-right: 0.3rem;
+  cursor: pointer;
 }
 
-.breadCrumb a:hover{
-	color:rgb(255, 92, 16);
+.breadCrumb a:hover {
+  color: rgb(255, 92, 16);
 }
 </style>

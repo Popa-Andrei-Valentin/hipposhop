7c3ebad492acaf03e7a3a4d845607cb4dad577a5
POPA Andrei-Valentin <Andrei-Valentin.POPA@cegedim.com>
2022-06-02 17:14:21 +0300

    Cart page implementation almost complete

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,38 +1,79 @@
 <template>
-  <div class="cartContainer">
+	<div class="cartContainer">
 		<div class="topContainer">
 			<h1>Your Cart</h1>
-		<button @click="closeCart()">X</button>
+			<button @click="closeCart()">X</button>
 		</div>
 		<div class="itemContainer">
-			<div class="itemList">
-				<p>Item test 1</p>
-				<p class="price">24.99$</p>
-				<p class="price">QTY:3</p>
-				<a>&#9747;</a>
-			</div>
-			<div class="itemList">
-				<p>Item test 2</p>
-				<p class="price">32.99$</p>
-				<p class="price">QTY:2</p>
-				<a>&#9747;</a>
+			<div class="itemList" v-for="item in cart" :key="item.id">
+				<p>{{item.title}}</p>
+				<p class="price">{{item.price}}$</p>
+				<p class="price">{{item.quantity}} {{item.unit}}</p>
+				<a @click="deleteCartItem(item)">&#9747;</a>
 			</div>
 			<p>+ Shipping: 1.99$</p>
 		</div>
 		<div class="checkOutContainer">
-			<h2>Total : 59,97 $</h2>
+			<h2>Total : {{this.totalPrice}} $</h2>
 			<button class="outBtn">Check Out</button>
 		</div>
 	</div>
+
 </template>
 
 <script>
+import { SHOP_KEY, TABLES } from "@/const";
 export default {
-	name:"ShoppingCartComponent",
-	emits:['closeCart'],
-	methods:{
-		closeCart(){
+	name: "ShoppingCartComponent",
+	emits: ['closeCart'],
+	
+	data() {
+		return {
+			cart: JSON.parse(this.$store.getters["cart/getList"]),
+			totalPrice: 0,
+		}
+	},
+	methods: {
+		closeCart() {
 			this.$emit('closeCart')
+		},
+		totalPriceDisplay(){
+			for(let item in this.cart){
+				console.log(item)
+				this.totalPrice = this.totalPrice + (this.cart[item].quantity * this.cart[item].price)
+			}
+			return this.totalPrice
+		},
+		deleteCartItem(item){
+			let cartOld =  JSON.parse(
+        localStorage.getItem(`${SHOP_KEY}-${TABLES.CART}`)
+      );
+			let index = cartOld.indexOf(item);
+			cartOld.splice(index,1)
+			localStorage.setItem(
+          `${SHOP_KEY}-${TABLES.CART}`,
+          JSON.stringify(cartOld)
+        );
+			this.cart=this.$store.getters["cart/getList"];
+		}
+		
+	},
+	computed: {
+    //  Retrieve list from LocalStorage.
+    cartItems() {
+      return this.$store.getters["cart/getList"]
+    }
+  },
+	mounted() {
+		this.$store.dispatch("cart/loadList", this.data)
+		this.totalPriceDisplay()
+		console.log(this.cartItems)
+	},
+	watch:{
+		cartUpdate(){
+			if(this.$store.getters["cart/getList"] != this.cartItems){
+				this.cartItems = this.$store.getters["cart/getList"];
+			}
 		}
 	}
 }
@@ -43,7 +84,7 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Kanit&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 
-.topContainer{
+.topContainer {
 	background: rgb(173, 58, 12);
 	display: flex;
 	flex-direction: row;
@@ -52,24 +93,24 @@ export default {
 	margin-bottom: 2rem;
 }
 
-.topContainer button{
-	position:absolute;
-	right:0;
-	margin-right:0.4rem;
+.topContainer button {
+	position: absolute;
+	right: 0;
+	margin-right: 0.4rem;
 	background-color: white;
 	color: rgb(173, 58, 12);
 	border: 2px white solid;
 	cursor: pointer;
 }
 
-.topContainer button:hover{
-	
+.topContainer button:hover {
+
 	background-color: red;
 	color: white;
 	border: 2px red solid;
 }
 
-h1{
+h1 {
 	color: white;
 	display: flex;
 	/* align-items: center;
@@ -77,23 +118,24 @@ h1{
 	font-family: 'Kanit', sans-serif;
 }
 
-h2{
+h2 {
 	font-family: 'Kanit', sans-serif;
 
 }
 
 .cartContainer {
-  position: fixed;
+	position: fixed;
 	z-index: 999;
 	width: 30vw;
 	height: 100vh;
 	margin: 0 0;
 	background-color: white;
-	right:0;
-	border: 2px solid rgb(173, 58, 12);;
+	right: 0;
+	border: 2px solid rgb(173, 58, 12);
+	;
 }
 
-.itemContainer{
+.itemContainer {
 	display: flex;
 	align-items: center;
 	justify-content: center;
@@ -101,7 +143,7 @@ h2{
 	font-family: 'Roboto', sans-serif;
 }
 
-.itemList{
+.itemList {
 	display: flex;
 	flex-direction: row;
 	align-items: center;
@@ -110,16 +152,16 @@ h2{
 	/* border: 2px solid black; */
 }
 
-.itemList a{
+.itemList a {
 	position: relative;
 	align-items: flex-end;
 	color: red;
-	border: black 2px solid; 
+	border: black 2px solid;
 	cursor: pointer;
 	font-weight: bold;
 }
 
-.itemList a:hover{
+.itemList a:hover {
 	color: white;
 	background-color: red;
 	border: red 2px solid;
@@ -127,9 +169,9 @@ h2{
 
 }
 
-.itemList p{
+.itemList p {
 	padding-right: 2rem;
-	
+
 }
 
 .checkOutContainer {
@@ -139,7 +181,7 @@ h2{
 	flex-direction: column;
 }
 
-.outBtn{
+.outBtn {
 	background-color: rgb(173, 58, 12);
 	font-family: 'Roboto', sans-serif;
 	border: none;
@@ -153,7 +195,7 @@ h2{
 	cursor: pointer;
 }
 
-.outBtn:hover{
-		background-color: rgb(219, 115, 74);
+.outBtn:hover {
+	background-color: rgb(219, 115, 74);
 }
 </style>
